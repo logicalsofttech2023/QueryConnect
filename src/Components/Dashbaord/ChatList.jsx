1673420a5a1491c3ad0553d731187ba2108a45a9
logicalsofttech2023 <logicalsofttech2023@gmail.com>
2025-10-16 19:02:35 +0530

    code

--- src/Components/Dashbaord/ChatList.jsx
+++ src/Components/Dashbaord/ChatList.jsx
@@ -277,7 +277,7 @@ const ChatList = ({
                 left: 0,
                 right: 0,
                 height: "2em",
-                backgroundColor: "linear-gradient(transparent, white)",
+                backgroundColor: "linear-gradient(transparent, white) !important",
                 pointerEvents: "none",
                 transition: "all 0.4s ease",
               }}
